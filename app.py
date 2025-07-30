f0e90acc8ec07e995ee5977ad51687aacf3a4f69
Pwlklr <pawelkelar@outlook.com>
2025-07-30 13:39:57 +0200

    Poprawa komunikacji z backend

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, UploadFile, Form, status
-from fastapi.responses import FileResponse, PlainTextResponse, JSONResponse
+from fastapi import FastAPI, Form, status
+from fastapi.responses import PlainTextResponse, JSONResponse
 import uuid
 import os
 import subprocess
@@ -13,14 +13,26 @@ def root():
 
 @app.post("/run")
 async def run_grapharna(uuid: str = Form(...), seed: int = Form(42)):
+    """
+    This function is the main FastAPI endpoint. It takes 2 parameters: uuid and seed.
+    First param is used to identify the job and locate necessery files in the shared volume.
+    Second param is the seed engine parameter.
 
-    EPOCHS = 800
+    Processing:
+    A shared volume is used between backend and the engine, in order to minimise necessery http data transfers
+    #1 Folder setup: we user 2 distinct folders: engine inputs, where backend creates the files and engine outputs
+    #2 we use the subprocess run command in order to calculate the .pdb results
+    #3 we check if the file was generated
+    #4 we run the annotator subprocess in order to generate output in fasta format
+    """
 
     input_path = f"/shared/samples/engine_inputs/{uuid}.dotseq"
     output_folder = f"/shared/samples/engine_outputs"
     output_name = uuid
 
-    output_path = os.path.join(output_folder, output_name + ".pdb")
+    output_path_pdb = os.path.join(output_folder, output_name + ".pdb")
+    output_path_json = os.path.join(output_folder, output_name + ".json")
+    output_path_dot = os.path.join(output_folder, output_name + ".dot")
 
 
     try:
@@ -33,23 +45,23 @@ async def run_grapharna(uuid: str = Form(...), seed: int = Form(42)):
         ], check=True)
 
         for _ in range(20):
-            if os.path.exists(output_path):
+            if os.path.exists(output_path_pdb):
                 break
             sleep(0.5)
 
-        if not os.path.exists(output_path):
-            print(f"Output file {output_path} can't be found or wasn't generated.")
+        if not os.path.exists(output_path_pdb):
+            print(f"Output file {output_path_pdb} can't be found or wasn't generated.")
             return JSONResponse(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content={"error": f"Output file {output_path} can't be found or wasn't generated."}
+                content={"error": f"Output file {output_path_pdb} can't be found or wasn't generated."}
             )
         
         try:
             subprocess.run([
                 "annotator",
-                "--json", str(os.path.join(output_folder, output_name + ".json")),
-                f"--dot",  str(os.path.join(output_folder, output_name + ".dot")),
-                f"--extended", str(output_path)
+                "--json", str(output_path_json),
+                f"--dot",  str(output_path_dot),
+                f"--extended", str(output_path_pdb)
             ], check=True)
         
         except subprocess.CalledProcessError as e:
@@ -60,8 +72,10 @@ async def run_grapharna(uuid: str = Form(...), seed: int = Form(42)):
             )
         
             
-
-        return FileResponse(output_path, media_type="text/plain", filename=f"{uuid}.pdb")
+        return_content = {"message": "OK", "pdbFilePath": output_path_pdb, 
+                          "jsonFilePath": output_path_json, "dotFilePath": output_path_dot}
+        
+        return JSONResponse(content=return_content, status_code=status.HTTP_200_OK)
 
     except subprocess.CalledProcessError as e:
         print(f"GraphaRNA engine failed")
